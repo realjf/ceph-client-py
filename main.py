20b92db2f3025fb258eaeec11ec290703e67da68
chenjiefeng <chenjiefeng@4399inc.com>
2019-02-26 18:14:28 +0800

    update

---
+++ main.py
@@ -0,0 +1,20 @@
+import rados
+
+try:
+    cluster = rados.Rados(conffile='')
+except TypeError as e:
+    print('Argument validation error: ', e)
+    raise e
+
+print("Created cluster handle.")
+
+try:
+    cluster.connect()
+except Exception as e:
+    print("connection error: ", e)
+    raise e
+finally:
+    print("Connected to the cluster.")
+
+
+
