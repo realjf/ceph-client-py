a451ea69383cc0b0dce66fe659050e3fc2a17fcd
chenjiefeng <chenjiefeng@4399inc.com>
2019-02-26 18:24:09 +0800

    update

--- main.py
+++ main.py
@@ -16,5 +16,43 @@ except Exception as e:
 finally:
     print("Connected to the cluster.")
 
+print("\n\nIO Context and Object Operations")
+print("==================================")
+
+print("\nCreating a context for the 'data' pool")
+if not cluster.pool_exists('data'):
+    raise RuntimeError('No data pool exists')
+ioctx = cluster.open_ioctx('data')
+
+print("\nWriting object 'hw' with contents 'Hello World!' to pool 'data'.")
+ioctx.write("hw", "Hello World!")
+print("Writing XATTR 'lang' with value 'en_US' to object 'hw'")
+ioctx.set_xattr("hw", "lang", "en_US")
+
+
+print("\nWriting object 'bm' with contents 'Bonjour tout le monde!' to pool 'data'.")
+ioctx.write("bm", "Bonjour tout le monde!")
+print("Writing XATTR 'lang' with value 'fr_FR' to object 'bm'")
+ioctx.set_xattr("bm", "lang", "fr_FR")
+
+print("\nContents of object 'hw'\n------------------------")
+print(ioctx.read("hw"))
+
+print("\n\nGetting XATTR 'lang' from object 'hw'")
+print(ioctx.get_xattr("hw", "lang"))
+
+print("\nContents of object 'bm'\n------------------------")
+print(ioctx.read("bm"))
+
+print("Getting XATTR 'lang' from object 'bm'")
+print(ioctx.get_xattr("bm", "lang"))
+
+
+print("\nRemoving object 'hw'")
+ioctx.remove_object("hw")
+
+print("Removing object 'bm'")
+ioctx.remove_object("bm")
+
 
 
